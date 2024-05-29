cf3e4bd0cb283f2c36395cad159b587c6f71ecd1
Madhvi Rana <madhvikrana@gmail.com>
2024-05-29 08:18:06 +0545

    commit98

--- makuraapp/frontend/src/components/leftbar/LeftBar.jsx
+++ makuraapp/frontend/src/components/leftbar/LeftBar.jsx
@@ -29,8 +29,64 @@ const LeftBar = () => {
             <span> Maddy </span>
           </div>
           <div className="item">
-            <img src="" alt="" />
+            <img src={Friends} alt="" />
             <span>Friends</span>
+          </div>
+          <div className="item">
+            <img src={Groups} alt="" />
+            <span>Groups</span>
+          </div>
+          <div className="item">
+            <img src={Market} alt="" />
+            <span>Marketplace</span>
+          </div>
+          <div className="item">
+            <img src={Watch} alt="" />
+            <span>Watch</span>
+          </div>
+          <div className="item">
+            <img src={Memories} alt="" />
+            <span>Memories</span>
+          </div>
+        </div>
+        <hr/>
+        <div className="menu">
+          <span>Your shortcuts</span>
+          <div className="item">
+            <img src={Events} alt="" />
+            <span>Events</span>
+          </div>
+          <div className="item">
+            <img src={Gaming} alt="" />
+            <span>Gaming</span>
+          </div>
+          <div className="item">
+            <img src={Gallery} alt="" />
+            <span>Gallery</span>
+          </div>
+          <div className="item">
+            <img src={Videos} alt="" />
+            <span>Videos</span>
+          </div>
+          <div className="item">
+            <img src={Messages} alt="" />
+            <span>Messages</span>
+          </div>
+        </div>
+        <hr />
+        <div className="menu">
+          <span>Others</span>
+          <div className="item">
+          <img src={Fund} alt="" />
+            <span>Fundraiser</span>
+          </div>
+          <div className="item">
+          <img src={Tutorials} alt="" />
+            <span>Tutorials</span>
+          </div>
+          <div className="item">
+          <img src={Courses} alt="" />
+            <span>Courses</span>
           </div>
         </div>
       </div>
