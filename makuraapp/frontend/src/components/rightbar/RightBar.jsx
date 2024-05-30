969a8bb06d27ec7fc9e64913305fcef96d890a4b
Madhvi Rana <madhvikrana@gmail.com>
2024-05-30 13:58:44 +0545

    commit96

--- makuraapp/frontend/src/components/rightbar/RightBar.jsx
+++ makuraapp/frontend/src/components/rightbar/RightBar.jsx
@@ -1,12 +1,162 @@
-import React from 'react'
-import "./rightbar.scss"
+import React from "react";
+import "./rightbar.scss";
 
 const RightBar = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="rightBar">
+      <div className="container">
+        <div className="item">
+          <span>Suggestions For You</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/22626145/pexels-photo-22626145/free-photo-of-a-woman-with-blonde-hair-and-a-lollipop.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+                alt=""
+              />
+              <span>Madhvi</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/22626145/pexels-photo-22626145/free-photo-of-a-woman-with-blonde-hair-and-a-lollipop.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+                alt=""
+              />
+              <span>Madhvi</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+        </div>
+        <div className="item">
+          <span>Latest Activities</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/22626145/pexels-photo-22626145/free-photo-of-a-woman-with-blonde-hair-and-a-lollipop.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+                alt=""
+              />
+              <p>
+                <span>Madhvi</span> changed their cover picture
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/22626145/pexels-photo-22626145/free-photo-of-a-woman-with-blonde-hair-and-a-lollipop.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+                alt=""
+              />
+              <p>
+                <span>Madhvi</span> changed their cover picture
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/22626145/pexels-photo-22626145/free-photo-of-a-woman-with-blonde-hair-and-a-lollipop.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+                alt=""
+              />
+              <p>
+                <span>Madhvi</span> changed their cover picture
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/22626145/pexels-photo-22626145/free-photo-of-a-woman-with-blonde-hair-and-a-lollipop.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+                alt=""
+              />
+              <p>
+                <span>Madhvi</span> changed their cover picture
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="item">
+          <span>Online Friends</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/22626145/pexels-photo-22626145/free-photo-of-a-woman-with-blonde-hair-and-a-lollipop.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+                alt=""
+              />
+              <div className="online"/>
 
-export default RightBar
+              <span>Madhvi</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/22626145/pexels-photo-22626145/free-photo-of-a-woman-with-blonde-hair-and-a-lollipop.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+                alt=""
+              />
+              <div className="online"/>
+
+              <span>Madhvi</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/22626145/pexels-photo-22626145/free-photo-of-a-woman-with-blonde-hair-and-a-lollipop.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+                alt=""
+              />
+              <div className="online"/>
+
+              <span>Madhvi</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/22626145/pexels-photo-22626145/free-photo-of-a-woman-with-blonde-hair-and-a-lollipop.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+                alt=""
+              />
+              <div className="online"/>
+
+              <span>Madhvi</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/22626145/pexels-photo-22626145/free-photo-of-a-woman-with-blonde-hair-and-a-lollipop.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+                alt=""
+              />
+              <div className="online"/>
+
+              <span>Madhvi</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/22626145/pexels-photo-22626145/free-photo-of-a-woman-with-blonde-hair-and-a-lollipop.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+                alt=""
+              />
+              <div className="online"/>
+
+              <span>Madhvi</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RightBar;
