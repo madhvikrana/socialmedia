c61bcbc85a974c12aff2253d7bff617f3dfd67b9
Madhvi Rana <madhvikrana@gmail.com>
2024-06-20 10:07:57 +0545

    commit81

--- makuraapp/frontend/src/pages/profile/Profile.jsx
+++ makuraapp/frontend/src/pages/profile/Profile.jsx
@@ -9,56 +9,65 @@ import PlaceIcon from "@mui/icons-material/Place";
 import LanguageIcon from "@mui/icons-material/Language";
 import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
-
+import Posts from "../../components/posts/Posts";
 
 const profile = () => {
-  return <div className="Profile">
-    <div className="images">
-      <img src="https://images.pexels.com/photos/7495924/pexels-photo-7495924.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load" alt="" className="cover" />
-      <img src="https://images.pexels.com/photos/25913490/pexels-photo-25913490/free-photo-of-h.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load" alt="" className="profilePic" />
-    </div>
-    <div className="profileContainer">
-      <div className="uInfo">
-        <div className="left">
-          <a href="https://www.facebook.com">
-            <FacebookTwoToneIcon fontSize="large" />
-          </a>
-          <a href="https://www.instagram.com">
-            <InstagramIcon fontSize="large" />
-          </a>
-          <a href="https://x.com/?lang=en">
-            <TwitterIcon fontSize="large" />
-          </a>
-          <a href="https://www.linkedin.com">
-            <LinkedInIcon fontSize="large" />
-          </a>
-          <a href="https://www.pinterest.com">
-            <PinterestIcon fontSize="large" />
-          </a>
-        </div>
-        <div className="center">
-          <span>Madhvi</span>
-          <div className="info">
-            <div className="item">
-              <PlaceIcon/>
-              <span>Nepal</span>
-            </div>
-            <div className="item">
-              <LanguageIcon/>
-              <span>Rana.dev</span>
-            </div>
-            
+  return (
+    <div className="Profile">
+      <div className="images">
+        <img
+          src="https://images.pexels.com/photos/7495924/pexels-photo-7495924.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+          alt=""
+          className="cover"
+        />
+        <img
+          src="https://images.pexels.com/photos/25913490/pexels-photo-25913490/free-photo-of-h.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+          alt=""
+          className="profilePic"
+        />
+      </div>
+      <div className="profileContainer">
+        <div className="uInfo">
+          <div className="left">
+            <a href="https://www.facebook.com">
+              <FacebookTwoToneIcon fontSize="large" />
+            </a>
+            <a href="https://www.instagram.com">
+              <InstagramIcon fontSize="large" />
+            </a>
+            <a href="https://x.com/?lang=en">
+              <TwitterIcon fontSize="large" />
+            </a>
+            <a href="https://www.linkedin.com">
+              <LinkedInIcon fontSize="large" />
+            </a>
+            <a href="https://www.pinterest.com">
+              <PinterestIcon fontSize="large" />
+            </a>
           </div>
-          <button>follow</button>
+          <div className="center">
+            <span>Madhvi</span>
+            <div className="info">
+              <div className="item">
+                <PlaceIcon />
+                <span>Nepal</span>
+              </div>
+              <div className="item">
+                <LanguageIcon />
+                <span>Rana.dev</span>
+              </div>
+            </div>
+            <button>follow</button>
+          </div>
+          <div className="right">
+            <EmailOutlinedIcon />
+            <MoreVertIcon />
+          </div>
         </div>
-        <div className="right">
-          <EmailOutlinedIcon/>
-          <MoreVertIcon />
-        </div>
-
+        <Posts />
       </div>
     </div>
-  </div>;
+  );
 };
 
 export default profile;
